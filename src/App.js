d176cbe377d3ad21749ceca8e31037101c094d8b
GUchechi <godswilluchendu7@gmail.com>
2022-09-25 21:51:52 +0100

    used useState and useEffect to create infinit scroll

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [boxes, setBoxes] = useState([{bg: "#777"}])
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setBoxes([...boxes, 
+        {bg:'#' + ((Math.random() * 0xffffff) << 0).toString(16)}
+      ])
+    }, 100);
+      return () => clearInterval(interval); 
+  },[boxes])
+
+
   return (
     <div className="hero">
-      <div className="box"><p>1</p></div>
+      {
+        boxes.map((box, index) => (
+          <div key={index} className="box" style={{background: box.bg}}>
+            <p>{index}</p>
+          </div>
+        ))
+      }
     </div>
   );
 }
